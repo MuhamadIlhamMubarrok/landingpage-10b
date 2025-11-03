18da12a2fe409a62cb6628f2b146e109a1b736f6
GarudaPratama <garuda281209@student.abudzar.sch.id>
2025-11-03 13:56:35 +0700

    Struktur Propre (ongoing)

--- src/components/ProjekPrestasi/Propre.jsx
+++ src/components/ProjekPrestasi/Propre.jsx
@@ -5,9 +5,14 @@ export default function Propre() {
 
 
   return (
-    <section>
-      <div></div>
-      <div></div>
+    <section className="w-24 md:w-36">
+      <div className="">
+        <h1 className="">Ini Project</h1>
+      </div>
+
+      <div className="">
+        <h1 className="">Ini Prestasi</h1>
+      </div>
     </section>
   )
 }
