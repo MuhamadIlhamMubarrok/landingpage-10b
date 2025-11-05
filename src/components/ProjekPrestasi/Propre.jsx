7d94fded44a1dc1fff8a68c7435c81003a827924
GarudaPratama <garuda281209@student.abudzar.sch.id>
2025-11-05 13:58:10 +0700

    Propre (ongoing)

--- src/components/ProjekPrestasi/Propre.jsx
+++ src/components/ProjekPrestasi/Propre.jsx
@@ -1,26 +1,64 @@
 
 import { motion } from "framer-motion"
+import img1 from "../../assets/2025.png"
+import img2 from "../../assets/2025.png"
+import img3 from "../../assets/2025.png"
+import img4 from "../../assets/2025.png"
+import img5 from "../../assets/2025.png"
+import img6 from "../../assets/2025.png"
+import img7 from "../../assets/2025.png"
 
 
 export default function Propre() {
   
-
+  const images = [
+   img1,
+   img2,
+   img3,
+   img4,
+   img5,
+   img6,
+   img7,
+  ]
 
   return (
-    <motion.section className="bg-secondary">
+    <motion.section className="bg-secondary items-center text-center justify-center flex flex-col min-h-screen">
+      <motion.h1
+        className="text-4xl font-hanson text-primary mb-12"
+          initial={{ opacity: 0, y: 30 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.7 }}
+          viewport={{ once: true }}
+      >
+        Ini Prestasi
+      </motion.h1>
       <motion.div
-        className="bg-secondary"
+        className="group w-[800px] overflow-x-auto whitespace-nowrap flex scrollbar-hide border border-gray-300 rounded-2xl p-4"
         initial={{ opacity: 0, y: 30 }}
         whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.7 }}
         viewport={{ once: true }}
       >
-        <motion.div>
-          <span></span>
+        <motion.div className="marquee inline-flex min-w-max">
+          {images.map((src, i) => (
+            <img  
+              key={i}
+              src={src}
+              alt=""
+              className="mx-12 h-28 w-28 object-cover rounded-xl"
+            />
+          ))}
         </motion.div>
 
-        <motion.div>
-          <span></span>
+        <motion.div className="marquee inline-flex min-w-max">
+          {images.map((src, i) => (
+            <img 
+              key={i + images.length}
+              src={src}
+              alt=""
+              className="mx-12 h-28 w-28 object-cover rounded-xl"
+            />
+          ))}
         </motion.div>
       </motion.div>
     </motion.section>
