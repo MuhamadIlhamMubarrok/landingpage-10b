6503108c77856aff44aee450770ce6985c429bba
MuhamadIlhamMubarrok <milhammubarrok@gmail.com>
2025-11-06 13:31:39 +0700

    fix data propre

--- src/components/ProjekPrestasi/Propre.jsx
+++ src/components/ProjekPrestasi/Propre.jsx
@@ -1,83 +1,46 @@
-
-import { motion } from "framer-motion"
-import img1 from "../../assets/2025.png"
-import img2 from "../../assets/2025.png"
-import img3 from "../../assets/2025.png"
-import img4 from "../../assets/2025.png"
-import img5 from "../../assets/2025.png"
-import img6 from "../../assets/2025.png"
-import img7 from "../../assets/2025.png"
-
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 
 export default function Propre() {
-  
-  const items = [
-   { img: img1, name: "Juara 3 Tahfizh 22/23" },
-   { img: img2, name: "Medali Emas Taekwondo Bigfest" },
-   { img: img3, name: "Juara 1 Tahfizh 24/25" },
-   { img: img4, name: "Top 8 IDN IT Fest" },
-   { img: img5, name: "Medali Emas Taekwondo KONI Bang Taja" },
-   { img: img6, name: "Ilham Mubarak" },
-   { img: img7, name: "Ilham Mubarak" },
-  ]
+  const [items, setItems] = useState([]);
 
-  const looped = [
-    ...items, ...items, ...items
-  ]
+  // ambil data dari JSON
+  useEffect(() => {
+    fetch("https://muhamadilhammubarrok.github.io/project-10b-assets/data/propre/propre.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setItems(data);
+      })
+      .catch((err) => console.error("Gagal mengambil data:", err));
+  }, []);
+
+  const looped = [...items, ...items, ...items];
 
   return (
     <motion.section className="bg-secondary items-center text-center justify-center flex flex-col min-h-screen">
-      <motion.h1
-        className="text-[6vw] md:text-[4vw] font-hanson text-primary mb-16"
-          initial={{ opacity: 0, y: 30 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.8 }}
-          viewport={{ once: true }}
-      >
+      <motion.h1 className="text-[6vw] md:text-[4vw] font-hanson text-primary mb-16" initial={{ opacity: 0, y: 30 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.8 }} viewport={{ once: true }}>
         Prestasi Kami
       </motion.h1>
-      <motion.div
-        className="group w-full overflow-x-auto whitespace-nowrap flex scrollbar-hide p-4"
-        initial={{ opacity: 0, y: 30 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.7 }}
-        viewport={{ once: true }}
-      >
+
+      <motion.div className="group w-full overflow-x-auto whitespace-nowrap flex scrollbar-hide p-4" initial={{ opacity: 0, y: 30 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 0.7 }} viewport={{ once: true }}>
         <motion.div className="marquee inline-flex min-w-max">
           {looped.map((src, i) => (
-            <div className="flex flex-col items-center mx-16">
-              <img 
-                key={i}
-                src={src.img}
-                alt={src.name}
-                className=" h-12 w-12 object-cover rounded-xl"
-              />
-
-              <h2 className="mt-6  font-hanson text-2xl text-primary">
-                {src.name}
-              </h2>
+            <div key={i} className="flex flex-col items-center mx-16">
+              <img src={src.img} alt={src.name} className="h-12 w-12 object-cover rounded-xl" />
+              <h2 className="mt-6 font-hanson text-2xl text-primary">{src.name}</h2>
             </div>
-            
           ))}
         </motion.div>
 
         <motion.div className="marquee inline-flex min-w-max">
           {looped.map((src, i) => (
-            <div className="flex flex-col items-center mx-16">
-              <img 
-              key={i + items.length}
-              src={src.img}
-              alt={src.name}
-              className=" h-12 w-12 object-cover rounded-xl"
-              />
-
-              <h2 className="mt-6  font-hanson text-primary text-2xl">
-                {src.name}
-              </h2>
+            <div key={i + items.length} className="flex flex-col items-center mx-16">
+              <img src={src.img} alt={src.name} className="h-12 w-12 object-cover rounded-xl" />
+              <h2 className="mt-6 font-hanson text-2xl text-primary">{src.name}</h2>
             </div>
           ))}
         </motion.div>
       </motion.div>
     </motion.section>
-  )
+  );
 }
