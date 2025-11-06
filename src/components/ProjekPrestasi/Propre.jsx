60a7f5ed09c14ac7fb54e5851396769a75aa1535
GarudaPratama <garuda281209@student.abudzar.sch.id>
2025-11-06 10:23:11 +0700

    Propre (ongoing)

--- src/components/ProjekPrestasi/Propre.jsx
+++ src/components/ProjekPrestasi/Propre.jsx
@@ -11,33 +11,33 @@ import img7 from "../../assets/2025.png"
 
 export default function Propre() {
   
-  const images = [
-   img1,
-   img2,
-   img3,
-   img4,
-   img5,
-   img6,
-   img7,
+  const items = [
+   { img: img1, name: "Juara 3 Tahfizh 2022/2023" },
+   { img: img2, name: "Ilham Mubarak" },
+   { img: img3, name: "Ilham Mubarak" },
+   { img: img4, name: "Ilham Mubarak" },
+   { img: img5, name: "Ilham Mubarak" },
+   { img: img6, name: "Ilham Mubarak" },
+   { img: img7, name: "Ilham Mubarak" },
   ]
 
   const looped = [
-    ...images, ...images, ...images
+    ...items, ...items, ...items
   ]
 
   return (
     <motion.section className="bg-secondary items-center text-center justify-center flex flex-col min-h-screen">
       <motion.h1
-        className="text-5xl font-hanson text-primary mb-12"
+        className="text-[4vw] font-hanson text-primary mb-16"
           initial={{ opacity: 0, y: 30 }}
           whileInView={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.7 }}
+          transition={{ duration: 0.8 }}
           viewport={{ once: true }}
       >
-        Ini Prestasi
+        Prestasi Kami
       </motion.h1>
       <motion.div
-        className="group w-[800px] overflow-x-auto whitespace-nowrap flex scrollbar-hide border border-gray-300 rounded-2xl p-4"
+        className="group w-full overflow-x-auto whitespace-nowrap flex scrollbar-hide p-4"
         initial={{ opacity: 0, y: 30 }}
         whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.7 }}
@@ -45,23 +45,36 @@ export default function Propre() {
       >
         <motion.div className="marquee inline-flex min-w-max">
           {looped.map((src, i) => (
-            <img  
-              key={i}
-              src={src}
-              alt=""
-              className="mx-12 h-28 w-28 object-cover rounded-xl"
-            />
+            <div className="flex flex-col items-center">
+              <img 
+                key={i}
+                src={src.img}
+                alt={src.name}
+                className="mx-32 h-12 w-12 object-cover rounded-xl"
+              />
+
+              <h2 className="mt-6 font-hanson text-2xl text-primary">
+                {src.name}
+              </h2>
+            </div>
+            
           ))}
         </motion.div>
 
         <motion.div className="marquee inline-flex min-w-max">
           {looped.map((src, i) => (
-            <img 
-              key={i + images.length}
-              src={src}
-              alt=""
-              className="mx-12 h-28 w-28 object-cover rounded-xl"
-            />
+            <div className="flex flex-col items-center">
+              <img 
+              key={i + items.length}
+              src={src.img}
+              alt={src.name}
+              className="mx-32 h-12 w-12 object-cover rounded-xl"
+              />
+
+              <h2 className="mt-6 font-hanson text-primary text-2xl">
+                {src.name}
+              </h2>
+            </div>
           ))}
         </motion.div>
       </motion.div>
